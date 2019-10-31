fc745094a047d833521d35a9e4d86359d3d06813
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2019-10-31 16:56:02 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export BOT_TOKEN='YOUR_TOKEN_HERE';
-export DB_ID='YOUR_ID_HERE';
-export DB_SECRET='YOUR_SECRET_HERE';
-export DB_TOKEN='YOUR_TOKEN_HERE';
+export BOT_TOKEN='946438606:AAFEI_QUW6qTGjQeI1oNxbGiPIID18sXuM8';
+export DB_ID='13egjl9cxzd1inf';
+export DB_SECRET='sn8xeb844qs4qf7';
+export DB_TOKEN='RlWJUsqVR9AAAAAAAAAAExpz16w5ovZ3X-Ettwng8xPSeyqKvkYqXgA81t8nzFAj';
 export NAVIGATION_ROOT='YOUR_PATH_HERE';
